a68df8a4170c6dd96a784af0d565fe0ea170f32a
Isaac A <isaacdarcilla@gmail.com>
2023-06-15 20:14:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align=" "><img src="https://github.com/isaacdarcilla/contesco-ui/blob/main/public/logo_96px.png" width="70"></p>
+
+## **UNILink**
+
+Seamless solution with Unilink — the hassle-free school management for modern university.
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'middle_name' => $this->faker->lastName(),
+            'last_name' => $this->faker->lastName(),
+            'prefix_name' => $this->faker->randomElement(['Mr.', 'Mrs.', 'Ms.', null]),
+            'suffix_name' => $this->faker->randomElement(['Jr.', 'Sr.', null]),
+            'phone_number' => $this->faker->phoneNumber(),
+            'phone_number_verified_at' => now(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+        ];
+    }
+
+    public function unverified(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}

72e94f8e2891421362289e4f7856d684abaa782c
Isaac A <isaacdarcilla@gmail.com>
2023-06-16 10:10:56 +0800

    fix(ui)!: forgot password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,12 +1,19 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+            <x-authentication-card-logo/>
+            <x-label class="text-center mx-auto font-bold text-xl mt-3">Seamless solution with <span
+                        class="text-border-black text-shadow-black text-gray-600">UNILink&trade;</span>
+            </x-label>
+            <div class="max-w-md mx-auto text-center mt-3">
+                <h1 class="text-center text-lg">Forgot password</h1>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+                <p class="mt-4 text-base text-denim-600">
+                    Forgot your password? No problem. Just let us know your email address, and we will email you a
+                    password reset link that will allow you to choose a new one.
+                </p>
+            </div>
+        </x-slot>
 
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
@@ -14,19 +21,26 @@
             </div>
         @endif
 
-        <x-validation-errors class="mb-4" />
+        <x-errors class="mb-4"/>
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <div class="block mt-2">
+                <x-label for="email" value="{{ __('Email') }}"/>
+                <x-input id="email" placeholder="you@email.com" class="block mt-1 w-full" type="text" name="email"
+                         :value="old('email')"
+                         autofocus autocomplete="username"/>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
+            <div class="flex items-center justify-between mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none"
+                   href="{{ route('login') }}">
+                    {{ __('Remember your password?') }}
+                </a>
+
+                <x-button type="submit">
+                    {{ __('Email Reset Link') }}
                 </x-button>
             </div>
         </form>

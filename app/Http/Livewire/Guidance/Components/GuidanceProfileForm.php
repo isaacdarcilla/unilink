9b0532670f2a0b4fa117a279e2f221617270db1f
Isaac A <isaacdarcilla@gmail.com>
2023-08-08 18:28:14 +0800

    wip: guidance profile form

--- app/Http/Livewire/Guidance/Components/GuidanceProfileForm.php
+++ app/Http/Livewire/Guidance/Components/GuidanceProfileForm.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Livewire\Guidance\Components;
 
+use App\Admin\Models\User;
+use App\Domain\Guidance\Models\GuidanceProfile;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class GuidanceProfileForm extends Component
 {
+    public ?User $user;
+
+    public ?GuidanceProfile $guidanceProfile;
+
     public function render(): View
     {
         return view('livewire.guidance.components.guidance-profile-form');
